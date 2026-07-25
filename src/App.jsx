8b8f9cfcd9ba827698c0ba9f73fdfc343d2ffed1
asp <asp@aptech.com>
2026-07-25 10:25:08 +0500

    Day - 01

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Home from './Home'
+import About from './About'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <h1>Hello React</h1>
-        <p>Some Work</p>
+        <h1>React App</h1>
+        <p>Welcome Back!</p>
         <Home></Home>
+        <About></About>
     </>
   )
 }
